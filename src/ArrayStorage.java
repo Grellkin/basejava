596aaba17d5b594a98bc7650e5bf68422064bde0
Anton Varganov <Varganov1998@yandex.ru>
2020-03-19 02:04:26 +0500

    done with task, hopefully

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,30 +1,52 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    Resume[] storage = new Resume[10000];
+    private Resume[] storage = new Resume[10000];
+    private int cursor;
 
+    //я видел комментарий про то что необходимо сделать именно null, но зачем??
     void clear() {
+        storage = null;
+        cursor = 0;
+        storage = new Resume[10000];
     }
 
     void save(Resume r) {
+        if (cursor == storage.length) System.out.println("Sorry, storage is full.");
+        else storage[cursor++] = r;
     }
 
     Resume get(String uuid) {
-        return null;
+        int index = searchPositionByUuid(uuid);
+        return index != -1 ? storage[index] : null;
     }
 
     void delete(String uuid) {
+        int index = searchPositionByUuid(uuid);
+        if (index != -1) {
+            System.arraycopy(storage, index + 1, storage, index, (--cursor) - index);
+        } else System.out.println("Sorry, can`t find element with id = " + uuid);
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
+
     Resume[] getAll() {
-        return new Resume[0];
+        return Arrays.copyOfRange(storage, 0, cursor);
     }
 
     int size() {
-        return 0;
+        return cursor;
+    }
+
+    private int searchPositionByUuid(String uuid) {
+        for (int i = 0; i < cursor; i++) {
+            if (storage[i].uuid.equals(uuid)) return i;
+        }
+        return -1;
     }
 }
